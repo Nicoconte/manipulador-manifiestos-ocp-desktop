3d79b270669efef7be846d3cac590830b61f51e3
NicoConte <nicoconte1999@gmail.com>
2022-12-19 01:04:37 -0300

    improve project table styles.

--- src/pages/Projects/ProjectDisplayer/ProjectTable/index.tsx
+++ src/pages/Projects/ProjectDisplayer/ProjectTable/index.tsx
@@ -12,22 +12,22 @@ export const ProjectTable = ({ projects }: ProjectTableProps) => {
 
     return (
         <div className="w-full h-full">
-            <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <table className="w-full text-md text-left text-gray-500 dark:text-gray-400">
+                <thead className="text-md text-slate-50">
                     <tr>
-                        <th scope="col" className="py-3 px-6 sticky top-0 bg-gray-50 dark:bg-gray-700">
+                        <th scope="col" className="py-3 px-6 sticky top-0 bg-slate-400 dark:bg-cyan-800">
                             Proyectos
                         </th>
                     </tr>
                 </thead>
                 <tbody>
                     {projects && projects.map((i, index) =>
-                        <tr onClick={() => navigate("/")} key={index} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                        <tr onClick={() => navigate("/")} key={index} className="bg-white border-b dark:border-b-cyan-900 dark:text-slate-50 transition ease-linear cursor-pointer hover:bg-slate-300 dark:hover:bg-cyan-700 dark:bg-cyan-900">
                             <td className="py-4 px-6">{i.name}</td>
                         </tr>
                     )}
                     {!projects.length &&
-                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                        <tr className="bg-white border-bdark:bg-cyan-800">
                             <td className="py-4 px-6 w-full flex justify-center items-center">
                                 <ArchiveBoxXMarkIcon className="h-6 mr-2" /> No se encontramos el proyecto que buscabas
                             </td>
